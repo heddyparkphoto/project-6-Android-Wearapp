873efeb116fc586c32a70501b2116feb997bb365
hyeryung park <hyeryungpark@hyeryungs-MacBook-Pro.local>
2017-02-16 00:27:42 -0500

    Project Ubiquitous: Copy starter Sunshine code and made it work on the phone emulator

---
+++ mobile/src/main/java/com/learn/heddy/sunshinewearever/SettingsActivity.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.learn.heddy.sunshinewearever;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        /*
+         * Normally, calling setDisplayHomeAsUpEnabled(true) (we do so in onCreate here) as well as
+         * declaring the parent activity in the AndroidManifest is all that is required to get the
+         * up button working properly. However, in this case, we want to navigate to the previous
+         * screen the user came from when the up button was clicked, rather than a single
+         * designated Activity in the Manifest.
+         *
+         * We use the up button's ID (android.R.id.home) to listen for when the up button is
+         * clicked and then call onBackPressed to navigate to the previous Activity when this
+         * happens.
+         */
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
